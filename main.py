866d6ea77d948d19c423a7d28c36a7981f0b8185
Ruth Carter <recarter07@gmail.com>
2016-09-14 21:23:09 -0500

    9.14.16 work on blog

--- main.py
+++ main.py
@@ -16,10 +16,67 @@
 #
 import webapp2
 import jinja2
+import os
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *args, **kw):
+        self.response.write(*args, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    post = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class Homepage(Handler):
+    def render_posts(self, title="", posts="")
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
+        self.render("posts.html", title=title, posts=posts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_posts()
+
+    def post(self):
+
+
+
+# separate render_front into new Handler class
+#   so that it doesn't render old posts on "make new post" page
+# will require making a new html form for new page "old posts directory"/homepg
+class MainHandler(Handler):
+    def render_front(self, title="", post="", error=""):
+        self.render("front.html", title=title, post=post, error=error)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        post = self.request.get("post")
+
+        if title and post:
+            a = Post(title = title, post = post)
+            a.put()
+
+            self.redirect("/")
+
+        else:
+            error = "We need both a title and a post!"
+            self.render_front(title, post, error)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
